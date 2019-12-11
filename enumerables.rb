6c2685c6469b7b4b25855c8f3146db4c0495c77e
Rory Hellier <rory.hellier@googlemail.com>
2019-12-11 17:18:20 -0500

    Finished writing and testing my_all method. Tested and working. merging into dev

--- enumerables.rb
+++ enumerables.rb
@@ -26,4 +26,17 @@ module Enumerable
     end
     temp
   end
+
+  def my_all?
+    valid = true
+    my_each do |k|
+      yield(k) ? next : valid = false
+      break
+    end
+    print valid
+  end
 end
+
+%w[ant, bear, panda].my_all? { |words| words.length > 1}
+
+[1, 2, 2, 5, 6, 245, 5, "a"].my_all? { |d| d.is_a? Integer}
