aca949721ab881cec11e6c9574723023bc5deda1
Rory Hellier <rory.hellier@googlemail.com>
2019-12-11 18:26:40 -0500

    Reverted changes to cinoy with gitflow

--- enumerables.rb
+++ enumerables.rb
@@ -36,15 +36,3 @@ module Enumerable
     end
     print valid
   end
-
-  def my_any?
-    bool = false
-    my_each do |any|
-      yield(any) ? bool = true : break
-    end
-    print bool
-  end
-end
-
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-array.my_all? {|x| x = 10}
