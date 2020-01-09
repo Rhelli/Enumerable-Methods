418f01443f9161e0752146f0aa032a9e8894a953
Rory Hellier <rory.hellier@googlemail.com>
2020-01-09 11:02:05 -0500

    Updated spec for stable version

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -3,35 +3,35 @@
 require './enumerables.rb'
 
 RSpec.describe Enumerable do
-  let(:arr) { Array.new(10) { rand(1..20) } }
-  let(:arr2) { [1, 2, 3, 4, 5] }
+  let(:rand_arr) { Array.new(10) { rand(1..20) } }
+  let(:set_array) { [1, 2, 3, 4, 5] }
   describe "#my_each" do
     it "returns to enumerator if no block given" do
-      expect(arr.my_each).to be_an Enumerator
+      expect(rand_arr.my_each).to be_an Enumerator
     end
 
     it "returns self when passed a block" do
-      expect(arr.my_each { |x| x }).to eql(arr)
+      expect(rand_arr.my_each { |x| x }).to eql(rand_arr)
     end
   end
 
   describe "#my_each_with_index" do
     it "returns to enumerator if no block given" do
-      expect(arr.my_each_with_index).to be_an Enumerator
+      expect(rand_arr.my_each_with_index).to be_an Enumerator
     end
 
     it "returns self with index if block given" do
-      expect(arr2.my_each_with_index { |x, y| ("#{x}, #{y}")}).to eql(arr2)
+      expect(set_array.my_each_with_index { |x, y| ("#{x}, #{y}")}).to eql(set_array)
     end
   end
 
   describe "#my_select" do
     it "returns to enumerator if no block given" do
-      expect(arr.my_select).to be_an Enumerator
+      expect(rand_arr.my_select).to be_an Enumerator
     end
 
     it "returns an array of elements for which the given block equal true" do
-      expect(arr2.my_select { |x| x.even? }).to eql([2, 4])
+      expect(set_array.my_select { |x| x.even? }).to eql([2, 4])
     end
   end
 
@@ -41,9 +41,13 @@ RSpec.describe Enumerable do
     end
 
     it "returns true if block never returns false or nil" do
-      expect(arr2.my_all? { |x| x.is_a?(Integer)})
+      expect(set_array.my_all? { |x| x.is_a?(Integer)})
     end
   end
 
-  describe "#"
+  describe "#my_none" do
+    it "returns true if block never returns true for all elements" do
+      expect ()
+    end
+  end
 end
